0759b196c6c2a61c097ac11c030045194b60165c
Sahidul Islam Jony <jonyahmed19@gmail.com>
2024-01-13 20:07:51 +0600

    solved

--- src/app/modules/project/project.model.ts
+++ src/app/modules/project/project.model.ts
@@ -1,37 +1,41 @@
-import { Schema, model } from 'mongoose';
-import { IProject, IProjectModel } from './project.interface';
+import {Schema, model} from 'mongoose';
+import {IProject, IProjectModel} from './project.interface';
 
 const projectSchema = new Schema<IProject>(
-  {
-    userId: {
-      type: Schema.Types.ObjectId,
-      ref: 'users',
+    {
+        userId: {
+            type: Schema.Types.ObjectId,
+            ref: 'users',
+        },
+        projectName: {
+            type: String,
+            required: true,
+        },
+        prompt: {
+            type: String,
+            required: true,
+        },
+        projectType: {
+            type: String,
+            required: true,
+        },
+        link: {
+            type: String,
+            unique: true,
+        },
+        image: {
+            type: String,
+        },
+        liveLink: {
+            type: String
+        }
+
     },
-    projectName: {
-      type: String,
-      required: true,
-    },
-    prompt: {
-      type: String,
-      required: true,
-    },
-    projectType: {
-      type: String,
-      required: true,
-    },
-    link: {
-      type: String,
-      unique: true,
-    },
-    image: {
-      type: String,
-    },
-  },
-  {
-    timestamps: true,
-    versionKey: false,
-    toJSON: { virtuals: true },
-  }
+    {
+        timestamps: true,
+        versionKey: false,
+        toJSON: {virtuals: true},
+    }
 );
 
 export const Project = model<IProject, IProjectModel>('Project', projectSchema);
